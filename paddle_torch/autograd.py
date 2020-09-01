5986deee1b56298c09e985bacca454ac63b8da0d
zzhang-QSI <zzhang@quantum-si.com>
2020-09-01 14:44:42 -0400

    implement autograd

---
+++ paddle_torch/autograd.py
@@ -0,0 +1,14 @@
+import paddle.fluid as fluid
+
+
+
+def grad(outputs, inputs, grad_outputs=None,retain_graph=False,
+        create_graph=True,  only_inputs=True,allow_unused=False):
+
+    return fluid.dygraph.grad(outputs, inputs,grad_outputs=grad_outputs,
+         retain_graph=retain_graph,
+         create_graph=create_graph,
+         only_inputs=only_inputs,
+         allow_unused=allow_unused,
+         no_grad_vars=None,
+         backward_strategy=None)
