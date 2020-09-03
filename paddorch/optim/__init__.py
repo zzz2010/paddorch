9549f6c37ee32453dc52d0d7104e430aa38a6a2e
zhizhuo <zzz2010@gmail.com>
2020-09-03 12:55:23 -0400

    optim class wrapper

--- paddorch/optim/__init__.py
+++ paddorch/optim/__init__.py
@@ -1,15 +1,17 @@
 from paddle.fluid.optimizer import  AdamOptimizer
 from paddle import fluid
 import paddorch.optim.lr_scheduler
-def Adam(params ,
+
+class Adam(AdamOptimizer):
+    def __init__(self,params ,
                     lr=0.001,
                     betas=None,
                     weight_decay=None,eps=1e-8,grad_clip=None,lazy_mode=False):
-    if betas is None:
-        betas=[0.9,0.999]
-    if weight_decay is None:
-        weight_decay=0.0
-    return AdamOptimizer( learning_rate=lr,
+        if betas is None:
+            betas = [0.9, 0.999]
+        if weight_decay is None:
+            weight_decay = 0.0
+        super(Adam, self).__init__(learning_rate=lr,
                  beta1=betas[0],
                  beta2=betas[1],
                  epsilon=eps,
@@ -19,3 +21,5 @@ def Adam(params ,
                  grad_clip=grad_clip,
                  name=None,
                  lazy_mode=lazy_mode)
+    def zero_grad(self):
+        self.clear_gradients()
