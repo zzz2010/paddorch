4e44a0e80feeb061d5c3999278f70546b69accaf
unknown <you@example.com>
2021-04-19 10:08:20 +0800

    add lodtensor2tensor

--- paddorch/nn/__init__.py
+++ paddorch/nn/__init__.py
@@ -548,8 +548,71 @@ class ModuleList(dygraph.LayerList):
 
 
 
-def Sequential(*layers):
-    return dygraph.Sequential(*layers)
+class Sequential(Layer):
+    """Sequential container.
+    Sub layers will be added to this container in the order of argument in the constructor.
+    The argument passed to the constructor can be iterable Layers or iterable name Layer pairs.
+    Parameters:
+        *layers(tuple): Layers or iterable name Layer pairs.
+    Examples:
+        .. code-block:: python
+            import paddle
+            import numpy as np
+            data = np.random.uniform(-1, 1, [30, 10]).astype('float32')
+            data = paddle.to_tensor(data)
+            # create Sequential with iterable Layers
+            model1 = paddle.nn.Sequential(
+                paddle.nn.Linear(10, 1), paddle.nn.Linear(1, 2)
+            )
+            model1[0]  # access the first layer
+            res1 = model1(data)  # sequential execution
+            # create Sequential with name Layer pairs
+            model2 = paddle.nn.Sequential(
+                ('l1', paddle.nn.Linear(10, 2)),
+                ('l2', paddle.nn.Linear(2, 3))
+            )
+            model2['l1']  # access l1 layer
+            model2.add_sublayer('l3', paddle.nn.Linear(3, 3))  # add sublayer
+            res2 = model2(data)  # sequential execution
+    """
+
+    def __init__(self, *layers):
+        super(Sequential, self).__init__()
+        if len(layers) > 0 and isinstance(layers[0], tuple):
+            for name, layer in layers:
+                self.add_sublayer(name, layer)
+        else:
+            for idx, layer in enumerate(layers):
+                self.add_sublayer(str(idx), layer)
+
+    def __getitem__(self, name):
+        if isinstance(name, slice):
+            return self.__class__(*(list(self._sub_layers.values())[name]))
+        else:
+            if name >= len(self._sub_layers):
+                raise IndexError('index {} is out of range'.format(name))
+            elif name < 0 and name >= -len(self._sub_layers):
+                name += len(self._sub_layers)
+            elif name < -len(self._sub_layers):
+                raise IndexError('index {} is out of range'.format(name))
+            return self._sub_layers[str(name)]
+
+    def __setitem__(self, name, layer):
+        assert isinstance(layer, Layer)
+        setattr(self, str(name), layer)
+
+    def __delitem__(self, name):
+        name = str(name)
+        assert name in self._sub_layers
+        del self._sub_layers[name]
+
+    def __len__(self):
+        return len(self._sub_layers)
+
+    def forward(self, input):
+        for layer in self._sub_layers.values():
+            input = layer(input)
+        return input
 
 
 class ConstantPad2d(Module):
--- paddorch/utils/dlpack.py
+++ paddorch/utils/dlpack.py
@@ -1,6 +1,36 @@
 from paddle import  fluid
+import numpy as np
+
+def to_lodtensor(data, place):
+  # 存储Tensor的长度作为LoD信息
+  seq_lens = [len(seq) for seq in data]
+  cur_len = 0
+  lod = [cur_len]
+  for l in seq_lens:
+      cur_len += l
+      lod.append(cur_len)
+  # 对待转换的 Tensor 降维
+  flattened_data = np.concatenate(data, axis=0).astype("int64")
+  flattened_data = flattened_data.reshape([len(flattened_data), 1])
+  # 为 Tensor 数据添加lod信息
+  res = fluid.LoDTensor()
+  res.set(flattened_data, place)
+  res.set_lod([lod])
+  return res
+
+def LodTensor_to_Tensor(lod_tensor):
+  # 获取 LoD-Tensor 的 lod 信息
+  lod = lod_tensor.lod()
+  # 转换成 array
+  array = np.array(lod_tensor)
+  new_array = []
+  # 依照原LoD-Tensor的层级信息，转换成Tensor
+  for i in range(len(lod[0]) - 1):
+      new_array.append(array[lod[0][i]:lod[0][i + 1]])
+  return new_array
 
 def to_dlpack(tensor):
+    tensor = to_lodtensor(tensor,fluid.CPUPlace())
     dltensor = tensor._to_dlpack()
     return dltensor
 
