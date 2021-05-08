e74199098d229163787986af18636045ae4588c6
Your Name <you@example.com>
2021-05-09 04:54:07 +0800

    no need convert tensor in dlpack

--- paddorch/utils/dlpack.py
+++ paddorch/utils/dlpack.py
@@ -12,26 +12,11 @@ def from_dlpack(dlpack):
     tensor_from_dlpack = fluid.core.from_dlpack(dlpack)
     place=tensor_from_dlpack._place()
     # print(ret)
-    # with paddle.fluid.dygraph.guard(place=place):
-    tensor_from_dlpack.__class__=paddle.fluid.LoDTensor
-    ret= paddle.Tensor( tensor_from_dlpack)
-    tensor_from_dlpack.__class__=paddle.fluid.core_avx.Tensor
-    ret= paddorch.convertTensor(ret)
-
-    # 
-    # if "cpu" in str(place).lower():
-    #     if "int64" in str(tensor_from_dlpack):
-    #         ret= paddorch.convertTensor(paddorch.Tensor(np.array(tensor_from_dlpack)).astype("int64"))
-    #     else:
-    #         ret= paddorch.Tensor(np.array(tensor_from_dlpack))
-    # else:
-    #     tensor_from_dlpack.__class__=paddle.fluid.LoDTensor
-    #     ret= paddle.Tensor( tensor_from_dlpack)
-    #     tensor_from_dlpack.__class__=paddle.fluid.core_avx.Tensor
-    #     ret= paddorch.convertTensor(ret)
-    #     print(ret)
-
-    # print(place,ret.place)
+    with paddle.fluid.dygraph.guard(place=place):
+        tensor_from_dlpack.__class__=paddle.fluid.LoDTensor
+        ret= paddle.Tensor( tensor_from_dlpack)
+        tensor_from_dlpack.__class__=paddle.fluid.core_avx.Tensor
+ 
     return ret
 
 
